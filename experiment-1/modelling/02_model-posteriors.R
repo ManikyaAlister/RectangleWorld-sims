89cfd4bf55f30451f8938f93bd1f5457a67e12e6
ManikyaAlister <alisterm@student.unimelb.edu.au>
2023-07-20 15:30:57 +1000

    script for hypothesis dists for alll blocks

--- experiment-1/modelling/02_model-posteriors.R
+++ experiment-1/modelling/02_model-posteriors.R
@@ -18,6 +18,15 @@ tb8  = filter(d_cartesian, block == 8)
 all_alpha_posteriors = fitAlphas(block = 8, data = tb8)
 save(all_alpha_posteriors, file = here("experiment-1/modelling/04_output/tb8-all-alpha-posteriors.Rdata"))
 
+# all blocks 
+blocks <- c(1,3,4,5,6,7)
+
+for (i in 1:length(blocks)){
+  block <- blocks[i]
+  d <- filter(d_cartesian, block == block)
+  all_alpha_posteriors <- fitAlphas(block = block, data = d)
+  save(all_alpha_posteriors, file = here(paste0("experiment-1/modelling/04_output/b",block,"-all-alpha-posteriors.Rdata")))
+}
 
 # Recursive alphas --------------------------------------------------------
 
